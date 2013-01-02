ead53c977bb817ebd4248218d049be79ba4a1e06
cor3ngine <matteo.michelini@gmail.com>
2013-01-02 18:01:11 +0100

    Version 0.0.3

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ FakeSSL impersonates an HTTPS server and prints the client requests.
 Download and unzip the master zip from github and execute the following into fakessl dir
 
     $ gem build ./fakessl.gemspec
-    $ gem install fakessl-0.0.1.gem
+    $ gem install fakessl-0.0.3.gem
  
 Or install it as:
 
@@ -27,6 +27,13 @@ Or install it as:
       [+] Client requests are: 
       => GET /byy.html HTTP/1.1
 
+In case you need to use it with a browser that is checking the authenticity 
+of the certificate you need to import the certificate as trusted.
+Firefox example.
+Go to Edit -> Preferences -> Advanced -> View Certificates -> Servers -> Import 
+-> Choose the certificate from your drive after generating it.
+Then click on the certificate and click on the button "Edit Trust..." inside 
+the Firefox window and enable "Trust the authenticity of this certificate"
 
 ## Contributing
 
--- lib/fakessl/fakessl.rb
+++ lib/fakessl/fakessl.rb
@@ -1,45 +1,69 @@
 require 'socket'
 require 'openssl'
 
-class FakeSSL 
+module FakeSSL
 
-  def initialize(hostname,lport)
-    @hostname = hostname
-    @lport = lport.to_i
-    $stdout.puts "[+] Generating fake SSL certificate for #{@hostname}"
-    generate_certificate
-    sslServer = server_setup
-    $stdout.puts "[+] Fake #{@hostname} is listening on port #{@lport}"
-    get_request(sslServer)
+  class << self; attr_accessor :domain, :cert, :key; end
+
+  FakeSSL.domain = nil
+  FakeSSL.cert = nil
+  FakeSSL.key = nil
+
+  def FakeSSL.cert_path(fcert, fkey)
+    FakeSSL.cert = fcert
+    FakeSSL.key = fkey
   end
 
-  def generate_certificate
-    %x[openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=#{@hostname}" -keyout #{@hostname}.key  -out #{@hostname}.cert]
-  end
-
-  def server_setup
-    server = TCPServer.new('localhost', @lport)
-    sslContext = OpenSSL::SSL::SSLContext.new
-    #certificate
-    sslContext.cert = OpenSSL::X509::Certificate.new(File.open("#{@hostname}.cert"))
-    #private key
-    sslContext.key = OpenSSL::PKey::RSA.new(File.open("#{@hostname}.key"))
-    sslServer = OpenSSL::SSL::SSLServer.new(server,sslContext)
-    return sslServer
-  end
-
-  def get_request(sslServer)
-    $stdout.puts "[+] Client requests are: "
-    loop do
-      conn = sslServer.accept
-      lineIn = conn.gets
-      if !lineIn.nil?
-        $stdout.puts "=> " + lineIn
-      end
+  class Cert
+    def initialize(domain)
+      FakeSSL.domain = domain
     end
+
+    public
+      def generate
+        %x[openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=#{FakeSSL.domain}" -keyout #{FakeSSL.domain}.key  -out #{FakeSSL.domain}.cert]
+      end
   end
 
-  private :generate_certificate, :server_setup, :get_request
+  class Server
+    def initialize(lport)
+      @lport = lport.to_i
+      sslServer = server_setup
+      puts "[+] Fake #{FakeSSL.domain} is listening on port #{@lport}"
+      get_request(sslServer)
+    end
+
+    private
+      def server_setup
+        server = TCPServer.new('localhost', @lport)
+        sslContext = OpenSSL::SSL::SSLContext.new
+        #certificate
+        if FakeSSL.cert.nil?
+          sslContext.cert = OpenSSL::X509::Certificate.new(File.open("#{FakeSSL.domain}.cert"))
+        else
+          sslContext.cert = OpenSSL::X509::Certificate.new(File.open("#{FakeSSL.cert}"))
+        end
+        #private key
+        if FakeSSL.key.nil?
+          sslContext.key = OpenSSL::PKey::RSA.new(File.open("#{FakeSSL.domain}.key"))
+        else
+          sslContext.key = OpenSSL::PKey::RSA.new(File.open("#{FakeSSL.key}"))
+        end
+        sslServer = OpenSSL::SSL::SSLServer.new(server,sslContext)
+        return sslServer
+      end
+
+      def get_request(sslServer)
+        puts "[+] Client requests are: "
+        loop do
+          conn = sslServer.accept
+          lineIn = conn.gets
+          if !lineIn.nil?
+            puts "=> " + lineIn
+          end
+        end
+      end
+  end
 
 end
 
